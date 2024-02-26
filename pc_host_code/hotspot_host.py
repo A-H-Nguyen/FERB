d7fe6dcb9554a31393438ef7ad3a3261b007d936
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-02-26 11:45:12 -0500

    addd more test code and created new directory for testing server

--- pc_host_code/hotspot_host.py
+++ pc_host_code/hotspot_host.py
@@ -1,139 +1,53 @@
 import argparse
-import asyncio
 import socket
 
 parser = argparse.ArgumentParser(description='Info for socket server')
 parser.add_argument('--ip', type=str, help='IP Address for Socket Server', required=True)
 parser.add_argument('--port', type=int, help='Port number for Socket Server', required=True)
-parser.add_argument('--debug', type=bool, help='Toggle debug mode on/off', required=False, default=False)
-parser.add_argument('-n', type=int, help='Expected number of FERBs for debugging', required=False, default=1)
 
 args = parser.parse_args()
 
+_IP = args.ip
+_PORT = args.port
 
-class FerbProtocol(asyncio.Protocol):
-    # This method is called when a new client connection is established
-    def connection_made(self, transport):
-        # Save a reference to the transport object
-        self.transport = transport
-        # Get the peer name of the client
-        self.peername = transport.get_extra_info("peername")
-        # Print a message
-        print(f"Connection from {self.peername}")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
 
-    # This method is called when data is received from the client
-    def data_received(self, data):
-        # Decode the data from bytes to string
-        message = data.decode()
-        print(f"Data received from {self.peername}")
-        
-        # Check data flag (first byte of message)
-        if message[0] == '~':
-            print(f"idk: {message}")
-        elif message[0] == '>':
-            print("Grid-EYE reading received")
+    _socket.bind((_IP, _PORT))
 
-    # This method is called when the client connection is closed
-    def connection_lost(self, exc):
-        print(f"Connection with {self.peername} closed")
-        # Close the transport
-        self.transport.close()
+    print(f"TCP server is listening on {(_IP, _PORT)}...\n")
 
+    _socket.listen(1)
+    connection, client_address = _socket.accept()
+    print(f"Connection established with {client_address}")
 
-class DebugServer:
-    def __init__(self, ip: str, port: int, num_conns: int) -> None:
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((args.ip, args.port))
+    while True:
+        try:
+            temps = []
 
-        print(f"TCP server is listening on {args.ip}:{args.port}...\n")
+            data = connection.recv(1024)
+            if not data:
+                raise Exception("Connection closed by client.")
+            
+            for i in range(0, len(data), 2):
+                pixel_value = data[i] | (data[i+1] << 8)
+                temps.append(pixel_value)
 
-        self.server.listen(num_conns)
-        connection, client_address = self.server.accept()
-        print(f"Connection established with {client_address}")
+                print(pixel_value, end=' ')
 
+                if (i + 2) % 16 == 0:
+                    print()
 
-    def send_msg(self):
-        msg = input("Message: ")
-        self.server.sendall(msg.encode())
-
-
-async def main(protocol_class):
-    # Get the current event loop
-    loop = asyncio.get_running_loop()
-
-    # Create a TCP server using the loop and the protocol class
-    server = await loop.create_server(protocol_class, args.ip, args.port)
-
-    # Get the server address and port
-    addr = server.sockets[0].getsockname()
-    print(f'Serving on {addr}')
-
-    async with server:
-        await server.serve_forever()
-
-
-if __name__ == "__main__":
-    try:
-        if args.debug:
-            server = DebugServer(args.ip, args.port, args.n)
-            while True:
-                try:
-                    server.send_msg()
-                except Exception as e:
-                    print(f"Error: {e}")
+                if len(temps) > 63:
+                    print()
                     break
-        else:
-            asyncio.run(main(FerbProtocol))
-        
-    except KeyboardInterrupt as k:
-        print("\nGoodbye cruel world\n")
 
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        except Exception as e:
+            print(f"Error: {e}")
 
-#         s.bind((args.ip, args.port))
+        except KeyboardInterrupt as k:
+            print("Server terminated by keyboard interrupt.")
+            connection.close()
+            break
 
-#         print(f"TCP server is listening on {(args.ip, args.port)}...\n")
-
-#         s.listen(1)
-#         connection, client_address = s.accept()
-#         print(f"Connection established with {client_address}")
-
-#         threading.Thread(target=handle_client, args=(connection,)).start()
-
-    #     while True:
-    #         try:
-    #             pass
-    #             # s.sendall(b'~hello from server')
-    #             # data = connection.recv(1024)
-    #             # if not data:
-    #             #     raise Exception("Connection closed by client.")
-                
-    #             # if data == bytearray(b'~ack'):
-    #             #     print(str(data))
-
-
-    #             # temps = []
-    #             # for i in range(0, len(data), 2):
-    #             #     pixel_value = data[i] | (data[i+1] << 8)
-    #             #     temps.append(pixel_value)
-
-    #             #     print(pixel_value, end=' ')
-
-    #             #     if (i + 2) % 16 == 0:
-    #             #         print()
-
-    #             #     if len(temps) > 63:
-    #             #         print()
-    #             #         break
-
-    #         except Exception as e:
-    #             print(f"Error: {e}")
-    #             break
-
-    #         except KeyboardInterrupt as k:
-    #             print("Server terminated by keyboard interrupt.")
-    #             connection.close()
-    #             break
-
-    #     s.close()
+    _socket.close()
 
