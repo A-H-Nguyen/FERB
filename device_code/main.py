8837416bc11a900ae726865a7fc495fc7a66756a
jeremy <kidjeremyat10@gmail.com>
2024-03-05 15:43:13 -0500

    led

--- device_code/main.py
+++ device_code/main.py
@@ -10,7 +10,7 @@ from debug import FerbCLI
 HOST_SSID = 'Ferbius'
 HOST_PASS = 'ajetaFERB'
 SERVER_IP = '10.42.0.1'  # IP address of the server, obtained from ifconfig output
-SERVER_PORT = 12345      # Arbitrary port number for the server, ensure it's known and available
+SERVER_PORT = 11111     # Arbitrary port number for the server, ensure it's known and available
 
 # Constants for Grid-EYE configuration
 SDA_PIN = 16
@@ -20,6 +20,8 @@ debug_pin = machine.Pin(12, machine.Pin.IN)
 led = machine.Pin("LED", machine.Pin.OUT)
 
 if __name__ == "__main__":
+
+    led.on()
 
     net = NetHandler()
     sensor = AMG88XX(machine.I2C(0, sda=SDA_PIN, scl=SCL_PIN, freq=400000))
@@ -45,33 +47,35 @@ if __name__ == "__main__":
 
         while True:
             if debug_mode:
-                if not cli.handle_input():
-                    break
+                # if not cli.handle_input():
+                #     break
+                pass
             else:
                 try:
                     sensor.refresh()
-                    time.sleep_ms(100)
+                    time.sleep_ms(1000)
 
-                    # for row in range(8):
-                    #     print()
-                    #     for col in range(8):
-                    #         print('{:4d}'.format(sensor[row, col]), end='')
-                    # print()
+                    for row in range(8):
+                        print()
+                        for col in range(8):
+                            print('{:4d}'.format(sensor[row, col]), end='')
+                    print("\n-------------------------")
 
                     data = sensor.get_buf()
-                    total = 0
-                    for i in range(0, 128, 2):
-                        total += data[i] | (data[i + 1] << 8)
-                    average = (total / 64) * _PIXEL_TEMP_CONVERSION
+                    net.send_to_socket(data)
+                    # total = 0
+                    # for i in range(0, 128, 2):
+                    #     total += data[i] | (data[i + 1] << 8)
+                    # average = (total / 64) * _PIXEL_TEMP_CONVERSION
 
-                    print(f"Average temp is {average} C\n")
+                    # print(f"Average temp is {average} C\n")
 
-                    if average > 25.0:
-                        led.on()
-                        net.send_to_socket(bytearray(str(average).encode()))
-                        time.sleep_ms(200)
-                    else:
-                        led.off()
+                    # if average > 22.0:
+                    #     led.on()
+                    #     net.send_to_socket(bytearray(str(average).encode()))
+                    #     time.sleep_ms(200)
+                    # else:
+                    #     led.off()
 
                 except Exception as e:
                     break
@@ -87,3 +91,5 @@ if __name__ == "__main__":
         time.sleep_ms(200)
 
         print("Ok, bye")
+
+        led.off()   
