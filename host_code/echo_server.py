acdec9390b9105474ae33b80c725c7e1131f626d
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-03-06 21:03:32 +0000

    I forgot to clean up the echo server earlier. Also added some stuff

--- host_code/echo_server.py
+++ host_code/echo_server.py
@@ -1,13 +1,6 @@
-import argparse
 import asyncio
 
 from server_base import CLI, FerbProtocol, ferb_main
-
-parser = argparse.ArgumentParser(description='Info for socket server')
-parser.add_argument('--ip', type=str, help='IP Address for Socket Server', required=True)
-parser.add_argument('--port', type=int, help='Port number for Socket Server', required=True)
-
-args = parser.parse_args()
 
 
 class echo_server(FerbProtocol):
