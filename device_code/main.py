c9658bf03796b4f984b69845b62a9174399d9e78
jeremy <kidjeremyat10@gmail.com>
2024-03-11 18:07:56 -0400

    hi

--- device_code/main.py
+++ device_code/main.py
@@ -18,8 +18,37 @@ SCL_PIN = 17
 debug_pin = machine.Pin(22, machine.Pin.IN)
 led = machine.Pin("LED", machine.Pin.OUT)
 
+<<<<<<< Updated upstream
 net = NetHandler()
 sensor = AMG88XX(machine.I2C(0, sda=SDA_PIN, scl=SCL_PIN, freq=400000))
+=======
+
+def calibrate_sensor(sensor, calibration_time=5):
+    print("Calibrating sensor for environmental temperature...")
+
+    total_pixel_values = 64
+    total = 0
+
+    start_time = time.ticks_ms()
+
+    while time.ticks_diff(time.ticks_ms(), start_time) < calibration_time * 1000:
+        sensor.refresh()
+        time.sleep_ms(1000)  # Adjust the sleep time if needed
+        pixels = sensor.get_buf()
+
+        for i in range(0, 128, 2):
+            total += pixels[i] | (pixels[i + 1] << 8)
+
+    average = (total / (total_pixel_values * calibration_time)) * _PIXEL_TEMP_CONVERSION
+
+    print(f"Enviromental temperature is {average} C")
+    print("Calibration Complete\n")
+
+    return average
+
+
+if __name__ == "__main__":
+>>>>>>> Stashed changes
 
 
 def get_average_temp(data) -> float:
@@ -90,4 +119,71 @@ if __name__ == "__main__":
     if debug_pin.value() < 1:
         FERB_debug()    
     else:
+<<<<<<< Updated upstream
         FERB_main()
+=======
+        debug_mode = False
+
+    try:
+        if debug_mode:
+            cli = FerbCLI(sensor=sensor, nethandler=net)
+        
+        else:
+            print("Check wi-fi connection:")
+            if not net.is_wifi_connected():
+                if not net.connect_to_wifi(HOST_SSID, HOST_PASS):
+                    raise Exception(f"Could not connect to wi-fi on {HOST_SSID}")
+            print(f"Connected to wi-fi on {net.get_ssid()}")
+
+            net.connect_to_socket(SERVER_IP, SERVER_PORT)
+
+        enviro = calibrate_sensor(sensor)
+        while True:
+            if debug_mode:
+                # if not cli.handle_input():
+                #     break
+                pass
+            else:
+                try:
+                    sensor.refresh()
+                    time.sleep_ms(1000)
+
+                    for row in range(8):
+                        print()
+                        for col in range(8):
+                            print('{:4d}'.format(sensor[row, col]), end='')
+                    print("\n-------------------------")
+
+                    data = sensor.get_buf()
+                    net.send_to_socket(data)
+                    # total = 0
+                    # for i in range(0, 128, 2):
+                    #     total += data[i] | (data[i + 1] << 8)
+                    # average = (total / 64) * _PIXEL_TEMP_CONVERSION
+
+                    # print(f"Average temp is {average} C\n")
+
+                    # if average > 22.0:
+                    #     led.on()
+                    #     net.send_to_socket(bytearray(str(average).encode()))
+                    #     time.sleep_ms(200)
+                    # else:
+                    #     led.off()
+
+                except Exception as e:
+                    break
+
+    except Exception as e:
+        machine.reset()
+
+    except KeyboardInterrupt as k:
+        net.disconnect_socket()
+        time.sleep_ms(200)
+        
+        net.disconnect_wifi()
+        time.sleep_ms(200)
+
+        print("Ok, bye")
+
+        led.off()   
+>>>>>>> Stashed changes
