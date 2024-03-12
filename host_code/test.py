1fef5d0055ee5658467b4359af6c6f84c298989f
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-03-12 20:20:06 +0000

    Removed deprecated interp func, and implemented runtime info printing

--- host_code/test.py
+++ host_code/test.py
@@ -1,32 +1,51 @@
 # DELETE ME LATER!
-
+import datetime
 import random as rand
 import numpy as np
+import time
 
-from scipy.interpolate import interp2d
+from scipy.interpolate import RectBivariateSpline
 
-# Original 8x8 matrix
-original_matrix = np.array([[rand.randint(0, 30) for _ in range(8)] for _ in range(8)])
+# Original matrix
+size = 8
+original_matrix = np.array([[rand.randint(0, 30) for _ in range(size)] for _ in range(size)])
 
-print(f"{original_matrix}\n-----------------------------------------")
+###############################################################################
+# These only needs to be called once!
 
-# Size of the new matrix
-N = 16
+coords = np.linspace(0, 7, size) # Generate x and y coordinates for the original matrix
 
-# Generate x and y coordinates for the original matrix
-x = np.linspace(0, 7, 8)
-y = np.linspace(0, 7, 8)
+bicubic_interp = RectBivariateSpline(coords, coords, original_matrix.T) # create interp func
 
-# Generate x and y coordinates for the new matrix
-new_x = np.linspace(0, 7, N)
-new_y = np.linspace(0, 7, N)
-
-# Create a function for bicubic interpolation
-bicubic_interp = interp2d(x, y, original_matrix, kind='cubic')
-
-# Perform bicubic interpolation to get the values for the new matrix
-new_matrix = bicubic_interp(new_x, new_y)
-
-print(f"{new_matrix}\n-----------------------------------------")
+###############################################################################
 
 
+def interp(new_size: int):
+    # Generate x and y coordinates for the new matrix
+    new_coords = np.linspace(0, 7, new_size)
+
+    # start_time = time.time()
+
+    # Perform bicubic interpolation to get the values for the new matrix
+    new_matrix = bicubic_interp(new_coords, new_coords).T
+
+    # elapsed_time = time.time() - start_time
+
+    # print(f"*** Interpolation for {new_size}-pixel grid\n", 
+    #       f" * Elapsed time: {elapsed_time} seconds\n")
+
+
+    # print(f"{new_matrix}\n-----------------------------------------")
+
+
+print(f"{datetime.datetime.now()}: Begin interpolation test")
+
+test_start = time.time()
+
+for test_num in range(100000):
+    interp(16)
+
+test_end = time.time()
+
+print(f"{datetime.datetime.now()}: Testing complete")
+print(f"Total test runtime: {test_end - test_start}")
