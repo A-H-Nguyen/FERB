80d508fe88e05ba9923bc65c62b07e2fc9f0550e
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-03-12 20:32:48 +0000

    Convert pixel values to celcius

--- host_code/grid_eye_printer.py
+++ host_code/grid_eye_printer.py
@@ -1,13 +1,13 @@
 import asyncio
 import numpy as np
 
-from server_base import FerbProtocol, Server
+from server_base import FerbProtocol, Server, PIXEL_TEMP_CONVERSION
 
 
 class GridEyeProtocol(FerbProtocol):
     def handle_data(self, data):
         # Convert the bytearray to a numpy array of 16-bit integers (short ints)
-        data_array = np.frombuffer(data, dtype=np.uint16)
+        data_array = np.frombuffer(data, dtype=np.uint16) * PIXEL_TEMP_CONVERSION
 
         # Reshape the array to form an 8x8 matrix
         matrix = data_array.reshape((8, 8))
