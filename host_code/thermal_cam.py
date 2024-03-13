87d66f08aaaf651a618c11995b7c48e48013ff42
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-03-13 14:56:43 +0000

    Improved the thermal camera drawing algorithm

--- host_code/thermal_cam.py
+++ host_code/thermal_cam.py
@@ -5,39 +5,60 @@ from graphics import *
 from server_base import FerbProtocol, Server, PIXEL_TEMP_CONVERSION
 from scipy.interpolate import RegularGridInterpolator
 
-
+# Number of pixels for both original Grid-EYE output, and interpolated output
 _GRID_LEN = 8
 _INTRP_LEN = 16
-_RESOLUTION = 400
 
 class ThermalCam:
-    def __init__(self) -> None:
-        # Define the size of the thermal image grid (8x8 for Grid-EYE)
-        self.win = GraphWin("Thermal Image", _RESOLUTION, _RESOLUTION)
+    def __init__(self, resolution=400, min_temp=15, max_temp=30, 
+                 low_color=(77, 255, 195), high_color=(255, 0, 0)) -> None:
+        # Define the size of the thermal image grid
+        self._RESOLUTION = resolution
 
-    def map_temperature(self, value):
-        # Define colors for different temperature ranges
-        ambient_temp = 22
-        person_temp = 23
+        # Set the window with the given resolution
+        self.win = GraphWin("Thermal Image", self._RESOLUTION, self._RESOLUTION)
 
-        if value < ambient_temp:            
-            return color_rgb(0, 255, 0) # green
-        
-        elif value > person_temp:
-            return color_rgb(255, 0, 0) # red
-        
-        else:
-            return color_rgb(255, 255, 0) # yellow
+        # Initialize temperature parameters
+        self._MIN_TEMP = min_temp  # Minimum temperature value
+        self._MAX_TEMP = max_temp  # Maximum temperature value
+        self._temp_diff = self._MAX_TEMP - self._MIN_TEMP
+
+        # Set color values
+        self._LOW_COLOR = low_color
+        self._HIGH_COLOR = high_color
+
+        # Compute color interpolation differences
+        self._red = self._HIGH_COLOR[0] - self._LOW_COLOR[0]
+        self._green = self._HIGH_COLOR[1] - self._LOW_COLOR[1]
+        self._blue = self._HIGH_COLOR[2] - self._LOW_COLOR[2]
+
+        # Precompute the length of grid-eye pixels for drawing
+        self._draw_distance = self._RESOLUTION / _GRID_LEN
+        # self._draw_distance = self._RESOLUTION / _INTRP_LEN
+
+    def map_temperature(self, val):
+        # Normalize val between 0 and 1
+        normalized_val = (val - self._MIN_TEMP) / self._temp_diff
+
+        # Interpolate RGB values
+        r = int(self._LOW_COLOR[0] + normalized_val * self._red)
+        g = int(self._LOW_COLOR[1] + normalized_val * self._green)
+        b = int(self._LOW_COLOR[2] + normalized_val * self._blue)
+
+        # Ensure that the color values are between 0 and 255
+        return color_rgb(np.clip(r, 0, 255),
+                         np.clip(g, 0, 255),
+                         np.clip(b, 0, 255))
 
     def draw_thermal_image(self, temps):
-        length, width = temps.shape
-
         # Draw squares to represent each pixel of the thermal image
-        for row in range(length):
-            for col in range(width):
+        for row in range(_GRID_LEN):
+        # for row in range(_INTRP_LEN):
+            for col in range(_GRID_LEN):
+            # for col in range(_INTRP_LEN):
                 color = self.map_temperature(temps[row, col])
-                rect = Rectangle(Point(col * (_RESOLUTION/width), row * (_RESOLUTION/length)), 
-                                 Point((col + 1) * (_RESOLUTION/width), (row + 1) * (_RESOLUTION/length)))
+                rect = Rectangle(Point(col * self._draw_distance, row * self._draw_distance), 
+                                 Point((col + 1) * self._draw_distance, (row + 1) * self._draw_distance))
                 rect.setFill(color)
                 rect.draw(self.win)
 
@@ -50,31 +71,38 @@ class ThermalCamProtocol(FerbProtocol):
         # Generate x and y coordinates for the original and interpolated Grid-EYE output
         self.orig_coords = np.linspace(0, _GRID_LEN-1, _GRID_LEN)
         self.new_coords = np.linspace(0, _GRID_LEN-1, _INTRP_LEN)
-        self.new_X, self.new_Y = np.meshgrid(self.new_coords, self.new_coords)
+        self.interp_X, self.interp_Y = np.meshgrid(self.new_coords, self.new_coords)
 
         self.cam = ThermalCam()
 
     def data_received(self, data): # redefine this to overwrite date-time printing
-        self.handle_data(data)
+        self.handle_data(data[:128])
         
         self.cancel_wait_timer()  # Cancel current wait timer
         self.start_wait_timer()  # Restart wait timer upon receiving data
 
     def handle_data(self, data):
         temps_array = np.frombuffer(data, dtype=np.uint16) * PIXEL_TEMP_CONVERSION
+        
+        # Check the size of our received data. This is neccessary if we run the Pico very fast,
+        # as in having a sleep of < 100 ms every time we send data.
+        # If we run the Pico very fat, then sometimes either the full 128 bytes will not be sent,
+        # or multiple packets can be received at once, making temps_array too large.
+        # Furthermore, for the current unoptimized drawing algorithm, a sleep of < 250 ms doesn't
+        # show a noticable performance increase.
+        if temps_array.size != 64:
+            print("skipped")
+            return
+        
         temps_matrix = temps_array.reshape((_GRID_LEN, _GRID_LEN))
-        temps_interp = self.interp(temps_matrix)
 
-        self.cam.draw_thermal_image(temps_interp)
-    
-    def interp(self, _grid):
-        # Create RegularGridInterpolator
-        interp_func = RegularGridInterpolator((self.orig_coords, self.orig_coords), _grid)
+        # # Create a scipy interpolation function for our temperature reading
+        # interp_func = RegularGridInterpolator((self.orig_coords, self.orig_coords), temps_matrix)
 
-        # Interpolate values for the new coordinates using RegularGridInterpolator
-        resized_matrix = interp_func((self.new_Y, self.new_X))
-
-        return resized_matrix
+        # # Interpolate values for the new coordinates using the interpolation function
+        # # and feed the interpolated matrix into the thermal cam
+        # self.cam.draw_thermal_image(interp_func((self.interp_Y, self.interp_X)))
+        self.cam.draw_thermal_image(temps_matrix)
 
 
     def connection_lost(self, exc):
