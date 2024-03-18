e1f9e82271aff95fc4d528d9447af679a5145989
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-03-18 22:37:35 +0000

    Continue testing optimizations

--- host_code/test_graphics.py
+++ host_code/test_graphics.py
@@ -1,5 +1,5 @@
-import asyncio
 import numpy as np
+import random as rand
 
 from graphics import *
 from server_base import FerbProtocol, Server, PIXEL_TEMP_CONVERSION
@@ -113,70 +113,13 @@ class ThermalCam:
                 if not rect.isDrawn():
                     rect.draw(self.win)
 
-class ThermalCamProtocol(FerbProtocol):
-    def __init__(self):
-        self.wait_timer = None
-        self.TIME_LIMIT = 10
-        self.temp_normalize_val = 0.00
+def value_gen():
 
-        # Generate x and y coordinates for the original and interpolated Grid-EYE output
-        self.orig_coords = np.linspace(0, _GRID_LEN-1, _GRID_LEN)
-        self.new_coords = np.linspace(0, _GRID_LEN-1, _INTRP_LEN)
-        self.interp_X, self.interp_Y = np.meshgrid(self.new_coords, self.new_coords)
-
-        self.cam = ThermalCam(min_temp=0, max_temp=10)
-
-    def data_received(self, data): # redefine this to overwrite date-time printing
-        self.handle_data(data[:128])
-        
-        self.cancel_wait_timer()  # Cancel current wait timer
-        self.start_wait_timer()  # Restart wait timer upon receiving data
-
-    def handle_data(self, data):
-        msg = data.decode()
-
-        if msg[0] == "~":
-            self.temp_normalize_val = float(msg[1:-1])
-            print("Hot Pixel:", self.temp_normalize_val, "Degrees C", "\n")
-            return
-        
-        # Convert the bytearray to a numpy array of 16-bit integers (short ints)
-        temps_array = np.frombuffer(data, dtype=np.uint16) * PIXEL_TEMP_CONVERSION
-                
-        # Check the size of our received data. This is neccessary if we run the Pico very fast,
-        # as in having a sleep of < 100 ms every time we send data.
-        # If we run the Pico very fat, then sometimes either the full 128 bytes will not be sent,
-        # or multiple packets can be received at once, making temps_array too large.
-        # Furthermore, for the current unoptimized drawing algorithm, a sleep of < 250 ms doesn't
-        # show a noticable performance increase.
-        if temps_array.size != 64:
-            print("skipped")
-            return
-        
-        temps_normalized = np.round(temps_array - self.temp_normalize_val).astype(int)
-
-        # temps_matrix = temps_array.reshape((_GRID_LEN, _GRID_LEN))
-        temps_matrix = temps_normalized.reshape((_GRID_LEN, _GRID_LEN))
-        
-        # Create a scipy interpolation function for our temperature reading
-        interp_func = RegularGridInterpolator((self.orig_coords, self.orig_coords), temps_matrix)
-
-        # Interpolate values for the new coordinates using the interpolation function
-        # and feed the interpolated matrix into the thermal cam
-        self.cam.draw_thermal_image(interp_func((self.interp_Y, self.interp_X)))
-        # self.cam.draw_thermal_image(temps_matrix)
-
-
-    def connection_lost(self, exc):
-        print(f"Connection with {self.peername} closed")
-        self.cam.win.close()
-        self.cancel_wait_timer()  # Cancel wait timer when connection is lost
 
 
 if __name__ == "__main__":
-    try:
-        server = Server()
-        asyncio.run(server.start_server(ThermalCamProtocol))
-        
-    except KeyboardInterrupt as k:
-        print("\nGoodbye cruel world\n")
+    orig_coords = np.linspace(0, _GRID_LEN-1, _GRID_LEN)
+    new_coords = np.linspace(0, _GRID_LEN-1, _INTRP_LEN)
+    interp_X, self.interp_Y = np.meshgrid(self.new_coords, self.new_coords)
+    cam = ThermalCam(min_temp=0, max_temp=10)
+
