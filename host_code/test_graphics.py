b831c4ced5eae08b9270b1ade6b0bb4cf8ab6b70
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-03-18 18:53:25 -0400

    Updated test script to draw randomly generated pixels

--- host_code/test_graphics.py
+++ host_code/test_graphics.py
@@ -1,13 +1,13 @@
 import numpy as np
-import random as rand
 
 from graphics import *
-from server_base import FerbProtocol, Server, PIXEL_TEMP_CONVERSION
-from scipy.interpolate import RegularGridInterpolator
+from time import sleep
 
-# Number of pixels for both original Grid-EYE output, and interpolated output
-_GRID_LEN = 8
 _INTRP_LEN = 16
+
+_MIN=15
+_MAX=30
+
 
 class ThermalCam:
     def __init__(self, resolution=400, min_temp=15, max_temp=30, 
@@ -30,18 +30,18 @@ class ThermalCam:
         self._blue = self._HIGH_COLOR[2] - self._LOW_COLOR[2]
 
         # Precompute the length of grid-eye pixels for drawing
-        # self._draw_distance = self._RESOLUTION / _GRID_LEN
-        self._draw_distance = self._RESOLUTION / _INTRP_LEN
+        # self._draw_dist = self._RESOLUTION / _GRID_LEN
+        self._draw_dist = self._RESOLUTION / _INTRP_LEN
 
-        # Create the Rectangle objects for thermal image once
-        self.rectangles = []
-        for row in range(_INTRP_LEN):
-            tmp_array = []
-            for col in range(_INTRP_LEN):
-                rect = Rectangle(Point(0, 0), Point(0, 0))  # Create a dummy rectangle
-                tmp_array.append(rect)
-            self.rectangles.append(tmp_array)
-        # self.curr_temps = np.zeros((16,16), dtype=np.uint16)
+        # # Create the Rectangle objects for thermal image once
+        # self.rectangles = []
+        # for row in range(_INTRP_LEN):
+        #     tmp_array = []
+        #     for col in range(_INTRP_LEN):
+        #         rect = Rectangle(Point(0, 0), Point(0, 0))  # Create a dummy rectangle
+        #         tmp_array.append(rect)
+        #     self.rectangles.append(tmp_array)
+        # # self.curr_temps = np.zeros((16,16), dtype=np.uint16)
 
         # Set the window with the given resolution
         self.win = GraphWin("Thermal Image", self._RESOLUTION, self._RESOLUTION)
@@ -65,6 +65,11 @@ class ThermalCam:
     def draw_thermal_image(self, temps):
         for row in range(_INTRP_LEN):
             for col in range(_INTRP_LEN):
+                color = self.map_temperature(temps[row, col])
+                rect = Rectangle(Point(col * self._draw_dist, row * self._draw_dist), 
+                                 Point((col + 1) * self._draw_dist, (row + 1) * self._draw_dist))                
+                rect.setFill(color)
+
                 # temp = temps[row, col]
 
                 # if not temp > 1:
@@ -74,52 +79,49 @@ class ThermalCam:
                 # #     color = color_rgb(*self._LOW_COLOR)
                 # color = self.map_temperature(temp)
 
-                # rect = Rectangle(Point(col * self._draw_distance, row * self._draw_distance), 
-                #                  Point((col + 1) * self._draw_distance, (row + 1) * self._draw_distance))
-                
                 # # rect = self.cam_pixels[row][col]
                 # color = self.map_temperature(temps[row, col])
                 
                 # rect.setFill(color)
                 # rect.draw(self.win)
 
-                # self.rect.move(col * self._draw_distance, row * self._draw_distance)
+                # self.rect.move(col * self._draw_dist, row * self._draw_dist)
                 # self.rect.setFill(color)
                 # self.rect.draw(self.win)
                 
                 # rect_index = row * _INTRP_LEN + col
                 # rect = self.rectangles[rect_index]
-                rect = self.rectangles[row][col]
+                # rect = self.rectangles[row][col]
 
-                temp = temps[row, col]
+                # temp = temps[row, col]
 
-                if temp > 1:
-                    color = self.map_temperature(temp)
-                else:
-                    color = color_rgb(*self._LOW_COLOR)
+                # if temp > 1:
+                #     color = self.map_temperature(temp)
+                # else:
+                #     color = color_rgb(*self._LOW_COLOR)
 
                 # color = self.map_temperature()
                 
-                rect.setFill(color)
-
                 # Update rectangle position and size
-                # rect.getP1().setX(col * self._draw_distance)
-                # rect.getP1().setY(row * self._draw_distance)
-                # rect.getP2().setX((col + 1) * self._draw_distance)
-                # rect.getP2().setY((row + 1) * self._draw_distance)
-                p1 = rect.getP1()
+                # rect.getP1().setX(col * self._draw_dist)
+                # rect.getP1().setY(row * self._draw_dist)
+                # rect.getP2().setX((col + 1) * self._draw_dist)
+                # rect.getP2().setY((row + 1) * self._draw_dist)
+                # p1 = rect.getP1()
 
-                # Draw the rectangle if it's not already drawn
-                if not rect.isDrawn():
-                    rect.draw(self.win)
+                # # Draw the rectangle if it's not already drawn
+                # if not rect.isDrawn():
+                #     rect.draw(self.win)
 
 def value_gen():
-
+    return np.random.randint(_MIN, _MAX, size=(_INTRP_LEN, _INTRP_LEN))
 
 
 if __name__ == "__main__":
-    orig_coords = np.linspace(0, _GRID_LEN-1, _GRID_LEN)
-    new_coords = np.linspace(0, _GRID_LEN-1, _INTRP_LEN)
-    interp_X, self.interp_Y = np.meshgrid(self.new_coords, self.new_coords)
     cam = ThermalCam(min_temp=0, max_temp=10)
+
+    while True:
+        cam.draw_thermal_image(value_gen())
+        sleep(0.033)
+
 
