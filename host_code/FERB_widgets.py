bd7f73a360fa2533562d4aadce206d1c33a744a8
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-04-05 20:41:47 -0400

    Clean up test scripts

---
+++ host_code/FERB_widgets.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+
+from tkinter import ttk
+
+class ScrollableFrame(tk.Frame):
+    def __init__(self, container, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
+
+        self.canvas = tk.Canvas(self)
+        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = tk.Frame(self.canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.scrollbar.grid(row=0, column=1, sticky="ns")
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+    def get_num_children(self) -> int:
+        return len(self.scrollable_frame.grid_slaves())
+    
+    def add_frame(self, frame):
+        frame_num = self.get_num_children() + 1
+        frame.grid(row= frame_num, column=0, sticky="ew")
+
+        print(f"Added frame number {frame_num}")
+
+    def get_frame(self, frame_id):
+        children = self.scrollable_frame.winfo_children()
+        for child in children:
+            if hasattr(child, 'id') and child.id == frame_id:
+                return child
+
+    def remove_frame_by_id(self, frame_id):
+        child = self.get_frame(frame_id)
+        child.pack_forget()
+        child.destroy()
+
+
+class ClientFrame(tk.Frame):
+    def __init__(self, container, client_id, client_name:str, btn_cmd = None, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
+
+        self.id = client_id
+
+        self.client_name = tk.Label(self, text=client_name)
+        self.person_count = tk.Label(self, text="Placeholder")
+
+        if not btn_cmd:
+            self.display_btn = tk.Button(self, text="Display", command=self.dummy)
+
+        self.client_name.grid(row=0, column=0, sticky="ew")
+        self.person_count.grid(row=1, column=0, sticky="ew")
+        self.display_btn.grid(row=0, column=1, columnspan=2, sticky="news")
+
+    def dummy(self):
+        pass
--- host_code/test_graphics.py
+++ host_code/test_graphics.py
@@ -6,6 +6,7 @@ import sys
 import threading
 import queue  # For thread-safe communication between threads
 
+from FERB_widgets import ScrollableFrame, ClientFrame
 from server_base import Server, FerbProtocol
 from thermal_cam import ThermalCam
 from tkinter import scrolledtext
@@ -89,123 +90,29 @@ class Redirect():
        pass
 
 
-class ScrollableFrame(tk.Frame):
-    def __init__(self, container, *args, **kwargs):
-        super().__init__(container, *args, **kwargs)
-
-        self.canvas = tk.Canvas(self)
-        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-        self.scrollable_frame = tk.Frame(self.canvas)
-
-        self.scrollable_frame.bind(
-            "<Configure>",
-            lambda e: self.canvas.configure(
-                scrollregion=self.canvas.bbox("all")
-            )
-        )
-
-        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
-
-        self.canvas.grid(row=0, column=0, sticky="nsew")
-        self.scrollbar.grid(row=0, column=1, sticky="ns")
-
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-
-    def get_num_children(self) -> int:
-        return len(self.scrollable_frame.grid_slaves())
-    
-    def add_frame(self, frame):
-        frame_num = self.get_num_children() + 1
-        frame.grid(row= frame_num, column=0, sticky="ew")
-
-        print(f"Added frame number {frame_num}")
-
-    def remove_frame_by_id(self, frame_id):
-        children = self.scrollable_frame.winfo_children()
-        for child in children:
-            if hasattr(child, 'id') and child.id == frame_id:
-                child.pack_forget()
-                child.destroy()
-                break
-
-
-class FERBClientFrame(tk.Frame):
-    def __init__(self, container, client_id, client_name:str, *args, **kwargs):
-        super().__init__(container, *args, **kwargs)
-
-        self.id = client_id
-        self.client_name = tk.Label(self, text=client_name)
-        self.data = tk.Label(self, text="Placeholder")
-
-        self.client_name.grid(row=0, column=0, sticky="ew")
-        self.data.grid(row=1, column=0, sticky="ew")
-
-
 class CamFrame(tk.Canvas):
     def __init__(self, container, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
         self.label = tk.Label(self, text="Hey")
         self.label.grid(row=0, column=0, sticky="ew")
 
+
 class ServerMonitor(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Asyncio Server Monitor")
 
-        # self.label = tk.Label(self, text=f"Server running on {_DEFAULT_IP}:{_DEFAULT_PORT}")
-        # self.label.pack()
-        # self.label.grid(row=0, column=0)
-
         self.clients = {0:0}
 
-        self.create_scrollable_gui()
         self.cam_fram = CamFrame(self)
         self.cam_fram.grid(row=1, column=1)
 
-        # self.client_incoming_queue_label = tk.Label(self, text="Connected Clients:")
-        # self.client_incoming_queue_label.grid(row=0, column=0)
-
-        # self.client_incoming_queue = scrolledtext.ScrolledText(self, width=40, height=10)
-        # self.client_incoming_queue.grid(row=0, column=1)
-
-        # # await self.start_server()
-        # # self.server = Server(lambda:GridEyeProtocool(self))
-        # # threading.Thread(target=self.server.run).start()
-        
-        # threading.Thread(target=self.watch_clients, args=(client_incoming_queue,)).start()
-
-    def create_scrollable_gui(self):
         self.scrollable_frame = ScrollableFrame(self)
         self.scrollable_frame.grid(row=1,column=0)
-        # for i in range(20):
-        #     frame = FERBClientFrame(self.scrollable_frame.scrollable_frame, 
-        #                             client_id=i, client_name=f"FERB{i}",
-        #                              relief="sunken", borderwidth=1)
-        #     frame.configure(width=100, height=50)
-        #     frame.grid_propagate(False)  # Prevents the frame from resizing to fit its contents
-        #     label = tk.Label(frame, text=f"Frame {i}")
-        #     label.pack(expand=True, fill="both")
-            # self.scrollable_frame.add_frame(frame)
-
-    # async def start_server(self):
-    #     # Get the current event loop
-    #     loop = asyncio.get_running_loop()
-
-    #     # Create a TCP server using the loop and the protocol class
-    #     self.server = await loop.create_server(GridEyeProtocol,
-    #                                            '10.42.0.1', 11111)
-    #     print(f"Serving on {self.server.sockets[0].getsockname()}", "\n")
-    #     
-    #     async with self.server:
-    #         await self.server.serve_forever()
-
+    
     def add_client(self, client_id):
-        # self.client_incoming_queue.insert(tk.END, message)
-        # self.client_incoming_queue.see(tk.END)
         client_name = f"FERB{self.scrollable_frame.get_num_children()}"
-        frame = FERBClientFrame(self.scrollable_frame.scrollable_frame,
+        frame = ClientFrame(self.scrollable_frame.scrollable_frame,
                                 client_id=client_id, client_name=client_name,
                                 relief="sunken", borderwidth=1)
         self.scrollable_frame.add_frame(frame)
@@ -216,22 +123,6 @@ class ServerMonitor(tk.Tk):
     def run(self):
         self.mainloop()
     
-    # def watch_clients(self, clients: queue.Queue):
-    #     print("The watcher has arrived")
-    #     while True:
-    #         if clients.empty():
-    #             continue
-    #         try:
-    #             with lock:
-    #                 print("New client detected")
-    #                 new_client = clients.get(timeout=10)
-    #                 self.add_client(new_client)
-
-    #         except Exception as e:
-    #             print(f"Error: {e}")
-    #             continue
-
-
 
 class ClientWatcher:
     def __init__(self):
@@ -247,20 +138,6 @@ class ClientWatcher:
                     print("\n-----------------------------------------\n")
             except:
                 continue
-
-
-   
-   
-   
-   
-   
-   
-def update_text(text_widget:tk.Text):
-    text_widget.delete("1.0", "end")
-
-    # Insert new text
-    new_text = "Hello, World!"
-    text_widget.insert("1.0", new_text)
 
 
 if __name__ == "__main__":
