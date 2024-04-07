9f0b560f8610813f0d21dc1bcd5f9f48b3d58077
Andrew Nguyen <a.h.nguyen475@gmail.com>
2024-04-07 13:29:06 -0400

    Fix thermal cam initialization

--- host_code/FERB_widgets.py
+++ host_code/FERB_widgets.py
@@ -5,8 +5,7 @@ from tkinter import ttk
 
 
 _GRID_LEN = 8
-# _GRAYSCALE = 255
-_THRESHOLD = 22
+_THRESHOLD = 23
 
 
 class ScrollableFrame(tk.Frame):
@@ -61,15 +60,24 @@ class ThermalCam(tk.Canvas):
 
         self.prev_image = np.zeros((_GRID_LEN, _GRID_LEN), dtype=bool)
         self.curr_image = np.zeros((_GRID_LEN, _GRID_LEN), dtype=bool)
+        
+        color = "black"
+
+        for row in range(_GRID_LEN):
+            for col in range(_GRID_LEN):
+
+                x0 = col * self._draw_len
+                y0 = row * self._draw_len
+                x1 = x0 + self._draw_len
+                y1 = y0 + self._draw_len
+
+                self.create_rectangle(x0, y0, x1, y1, fill=color, outline='')
 
     def draw_bw_image(self, temps):
         """
         Draw the thermal image in black and white. Only pixels over a certain
         theshold are shown. If a pixel is hot enough, it appears as white.
         """
-        # self.create_rectangle(0, 0, self.winfo_width(), self.winfo_height(), 
-                            #   fill="black", outline='')
-        print(self.prev_image)
         for row in range(_GRID_LEN):
             for col in range(_GRID_LEN):
                 
